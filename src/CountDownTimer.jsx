816460557fa6f9fe69fd4a24bf3cc6ee4ecd2bc1
chintanp-dev <chintan.dev@tech.mekanism.cc>
2023-12-04 18:21:07 +0530

    row column refactore

--- src/CountDownTimer.jsx
+++ src/CountDownTimer.jsx
@@ -12,18 +12,8 @@ import "./CountDownTimer.css";
 //   );
 // }
 
-function CountDownTimer() {
 
-  let refHour = useRef();
-  let refMinute = useRef();
-  let refSecond = useRef();
-  const timerIdRef = useRef(null);
-
-
-  const numOfRow = 5;
-  const numOfColumn = 6;
-  const numOfRowCol = numOfRow * numOfColumn;
-
+function CountDownTimer({onTimerComplete}) {
 
   const [inputValue, setInputValue] = useState({
     hour: "",
@@ -32,52 +22,10 @@ function CountDownTimer() {
   });
 
 
-  const [boxStates, setBoxStates] = useState(Array(numOfRowCol).fill(false));
-  const [showCountdownTimer, setShowCountdownTimer] = useState(false);
-  const [countdownPosition, setCountdownPosition] = useState({
-    left: 0,
-    top: 0,
-  });
-
-
-  const onBoxClick = (index, event) => {
-
-    setBoxStates((prevStates) => {
-      const newStates = [...prevStates];
-      newStates[index] = !newStates[index];
-      return newStates;
-    });
-
-    // for Positioning of inputs
-    const buttonRect = event.target.getBoundingClientRect();
-    setCountdownPosition({
-      left: buttonRect.left,
-      top: buttonRect.top + buttonRect.height,
-    });
-
-    setShowCountdownTimer(true);
-  };
-
-
-  const inputChangeOnBook = (event) => {
-    event.preventDefault();
-    // refHour.current.value = 0;
-    // refMinute.current.value = 0;
-    // refSecond.current.value = 0;
-
-    startTimer();
-  };
-
-
-  const onChangeHandler = (event) => {
-    const value = Number(event.target.value);
-    if (!isNaN(value)) {
-      setInputValue((prevVal) => ({
-        ...prevVal,
-        [event.target.name]: value,
-      }));
-    }
-  };
+  // let refHour = useRef();
+  // let refMinute = useRef();
+  // let refSecond = useRef();
+  const timerIdRef = useRef(null);
 
 
   const startTimer = () => {
@@ -104,109 +52,163 @@ function CountDownTimer() {
   };
 
 
-  const stopTimer = () => {
-    clearInterval(timerIdRef.current);
+  const inputChangeOnBook = (event) => {
+    event.preventDefault();
+    // refHour.current.value = 0;
+    // refMinute.current.value = 0;
+    // refSecond.current.value = 0;
+
+    startTimer();
   };
 
 
-  useEffect(() => {
-    return () => {
-      stopTimer();
-    };
-  }, []);
+  const onChangeHandler = (event) => {
+    const value = Number(event.target.value);
+    if (!isNaN(value)) {
+      setInputValue((prevVal) => ({
+        ...prevVal,
+        [event.target.name]: value,
+      }));
+    }
+  };
 
 
-  // const [boxStates, setBoxStates] = useState(Array(numOfRowCol).fill(false));
+  const stopTimer = () => {
+    clearInterval(timerIdRef.current);
+    onTimerComplete()
+  };
 
-  // const onBoxClickState = (index) => {
-  //   setBoxStates((prevStates) => {
-  //     const newStates = [...prevStates];
-  //     newStates[index] = !newStates[index];
-  //     return newStates;
-  //   });
-  // };
+
+  // useEffect(() => {
+  //   return () => {
+  // stopTimer();
+  // onTimerComplete()
+  //   };
+  // }, []);
+
 
   return (
     <>
-      {showCountdownTimer && (
-        <div
-          className="countdown-timer-input"
-          style={{
-            position: "absolute",
-            left: countdownPosition.left,
-            top: countdownPosition.top,
-          }}
-        >
-          {/* <div>
-            <span id="hour">{inputValue.hour}</span> :
-            <span id="minute"> {inputValue.minute}</span> :
-            <span id="second"> {inputValue.second}</span>
-          </div> */}
+      <div className="countdown-timer-input">
 
-          <form action="" className="center" onSubmit={inputChangeOnBook}>
-            Book For
-            <input
-              className="time-input"
-              type="number"
-              placeholder="hour"
-              name="hour"
-              value={inputValue.hour}
-              ref={refHour}
-              onChange={onChangeHandler}
-              max={23}
-            />
-            <input
-              className="time-input"
-              type="number"
-              placeholder="minute"
-              name="minute"
-              value={inputValue.minute}
-              ref={refMinute}
-              onChange={onChangeHandler}
-              max={59}
-            />
-            <input
-              className="time-input"
-              type="number"
-              placeholder="second"
-              name="second"
-              value={inputValue.second}
-              ref={refSecond}
-              onChange={onChangeHandler}
-              max={59}
-            />
-            <button className="book-button" type="submit">
-              Book
-            </button>
-          </form>
-        </div>
-      )}
+        <form action="" className="center" onSubmit={inputChangeOnBook}>
+          Book For
+          
+          <input
+            className="time-input"
+            type="number"
+            placeholder="hour"
+            name="hour"
+            value={inputValue.hour}
+            // ref={refHour}
+            onChange={onChangeHandler}
+            max={23}
+          />
 
-      <table>
-        <tbody>
-          {[...Array(numOfRow).keys()].map((row) => (
-            <tr key={row}>
-              {[...Array(numOfColumn).keys()].map((column) => {
-                const index = row * numOfColumn + column;
-                return (
-                  <td key={column}>
-                    <button
-                      className={`table-buttons ${
-                        boxStates[index] ? "invisible" : ""
-                      }`}
-                      onClick={(event) => onBoxClick(index, event)}
-                    >
-                      Table <br /> {index + 1}
-                    </button>
-                  </td>
-                );
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <input
+            className="time-input"
+            type="number"
+            placeholder="minute"
+            name="minute"
+            value={inputValue.minute}
+            // ref={refMinute}
+            onChange={onChangeHandler}
+            max={59}
+          />
+
+          <input
+            className="time-input"
+            type="number"
+            placeholder="second"
+            name="second"
+            value={inputValue.second}
+            // ref={refSecond}
+            onChange={onChangeHandler}
+            max={59}
+          />
+
+          <button className="book-button" type="submit">
+            Book
+          </button>
+
+        </form>
+      </div>
     </>
   );
 }
 
-export default CountDownTimer;
+function TableButtons({ index, boxStates, onBoxClick, onTimerComplete }) {
+  return (
+    <td>
+      <button
+        className={`table-buttons ${boxStates[index] ? "invisible" : ""}`}
+        onClick={() => onBoxClick(index)}
+      >
+        Table <br /> {index + 1}
+      </button>
+      {boxStates[index] && (
+        <CountDownTimer onTimerComplete={() => onTimerComplete(index)} />
+      )}
+    </td>
+  );
+}
+
+function TableBox() {
+    const numOfRow = 5;
+    const numOfColumn = 6;
+  
+    const [boxStates, setBoxStates] = useState(Array(numOfRow * numOfColumn).fill(false));
+  
+    const onBoxClick = (index) => {
+      setBoxStates((prevStates) => {
+        const newStates = [...prevStates];
+        newStates[index] = !newStates[index];
+        return newStates;
+      });
+    };
+  
+    const onTimerComplete = (index) => {
+      setBoxStates((prevStates) => {
+        const newStates = [...prevStates];
+        newStates[index] = false;
+        return newStates;
+      });
+    };
+  
+    const TableRow = () => {
+      return [...Array(numOfRow).keys()].map((row) => (
+        <tr key={row}>
+          {TableColumns(row)}
+        </tr>
+      ));
+    };
+  
+    const TableColumns = (row) => {
+      return [...Array(numOfColumn).keys()].map((column) => {
+        const index = row * numOfColumn + column;
+        return (
+          <TableButtons
+            key={column}
+            index={index}
+            boxStates={boxStates}
+            onBoxClick={onBoxClick}
+            onTimerComplete={onTimerComplete}
+          />
+        );
+      });
+    };
+  
+    return (
+      <>
+        <table>
+          <tbody>
+            {TableRow()}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+  
+  export default TableBox;
+
+
